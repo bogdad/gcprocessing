86284ff636315a6b71d4d95e779bd3f6d867eb6e
Vladimir Shakhov <vladimir@spotify.com>
2017-05-06 01:18:55 +0200

    added julia code to add them to one dataframe

---
+++ plotgcgraphs.jl
@@ -0,0 +1,29 @@
+Pkg.add("DataFrames")
+Pkg.add("JLD")
+
+using DataFrames
+using JLD
+
+function host(fullfilename)
+    _, filename = splitdir(fullfilename)
+    rsplit(split(filename, ".")[1], "-")[end]
+end
+
+function frame(fullfilename)
+    df = DataFrames.readtable(fullfilename, separator = ',', header = true)
+    df[:Host] = host(fullfilename)
+    df
+end
+
+function readalltoone(dir) 
+    overAll=DataFrames.DataFrame()
+    for file = readdir(dir)
+        println(file) # path to files
+        df = frame("$dir/$file")
+        overAll = [overAll; df]
+    end
+    overAll    
+end
+
+new = readalltoone("./new_csv")
+save("./workspace.jld", "new", new)
