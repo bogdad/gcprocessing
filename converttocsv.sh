a91d611ba51c9fb14bb51420041bd699d2c1a197
Vladimir Shakhov <vladimir@spotify.com>
2017-05-05 22:20:16 +0200

    cc

--- converttocsv.sh
+++ converttocsv.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 set -e
 SRC=${1}
-mkdir -p ${2}
+DEST=${2}
+mkdir -p ${DEST}
 FILES="${SRC}/*.log"
 for f in $FILES
 do
   echo "Processing $f file..."
   filename="${f##*/}"
-  java -jar ./gcviewer-1.35-SNAPSHOT.jar $f ${2}/processed_$filename.csv -t CSV
+  java -jar ./gcviewer-1.35-SNAPSHOT.jar $f ${DEST}/processed_$filename.csv -t CSV
 done
