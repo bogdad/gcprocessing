463de2faf09dd14429c96afbd4aa01111941c88e
Vladimir Shakhov <vladimir@spotify.com>
2017-05-06 13:06:11 +0200

    old date

--- mergedataframes.jl
+++ mergedataframes.jl
@@ -1,7 +1,9 @@
 Pkg.add("DataFrames")
+Pkg.add("DataFramesMeta")
 Pkg.add("JLD")
 
 using DataFrames
+using DataFramesMeta
 using JLD
 
 function host(fullfilename)
@@ -9,17 +11,18 @@ function host(fullfilename)
     rsplit(split(filename, ".")[1], "-")[end]
 end
 
-function frame(fullfilename)
+function dframe(fullfilename)
     df = DataFrames.readtable(fullfilename, separator = ',', header = true)
     df[:Host] = host(fullfilename)
-    df
+    dfWithTime = @transform(df, Time = Dates.unix2datetime(:Timestamp_unix_))
+    dfWithTime
 end
 
 function readalltoone(dir) 
     overAll=DataFrames.DataFrame()
     for file = readdir(dir)
         println(file) # path to files
-        df = frame("$dir/$file")
+        df = dframe("$dir/$file")
         overAll = [overAll; df]
     end
     overAll    
@@ -27,3 +30,6 @@ end
 
 new = readalltoone("./new_csv")
 writetable("output_new.csv", new)
+
+old = readalltoone("./old_csv")
+writetable("output_old.csv", old)
